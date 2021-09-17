9ab2e18aeda9847f113710e5500f43efb311499c
Filip Koza <peacefilip1989@gmail.com>
2021-09-17 15:50:41 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+// import Button from "@mui/material/Button";
+import { Route } from "react";
+
+function App() {
+  return (
+    <>
+      <header>
+        <a href="">
+          <img src="" alt="" />
+        </a>
+      </header>
+    </>
+  );
+}
+
+export default App;
